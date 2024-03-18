7351ae1b62defb0ff3e8309bbea4e2805377d66a
feverdreme <jeffreyhuang08@gmail.com>
2024-03-17 22:35:35 -0400

    Change ws support

--- client/components/Console.tsx
+++ client/components/Console.tsx
@@ -6,7 +6,7 @@ import ConsoleInput from "./ConsoleInput";
 import "./Console.css";
 
 export default function MachineLog() {
-  const WS_URL = "ws://127.0.0.1:800";
+  const WS_URL = "ws://127.0.0.1:8765";
   const { lastJsonMessage, readyState, sendJsonMessage } = useWebSocket(WS_URL, {
     shouldReconnect: (_closeEvent) => true,
   });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <RecoilRoot>
       <MachineLog />
-      {/* <CameraFeed /> */}
+      <CameraFeed />
     </RecoilRoot>
     </>
   );
