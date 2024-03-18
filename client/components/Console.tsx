753c2a0b87c56614c03accef3c951c1e14f1ab80
feverdreme <jeffreyhuang08@gmail.com>
2024-03-17 22:23:59 -0400

    Archictecture Changes

---
+++ client/components/Console.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Box from "@mui/material/Box";
+import useWebSocket from "react-use-websocket";
+import ConsoleLog from "./ConsoleLog";
+import ConsoleInput from "./ConsoleInput";
+import "./Console.css";
+
+export default function MachineLog() {
+  const WS_URL = "ws://127.0.0.1:800";
+  const { lastJsonMessage, readyState, sendJsonMessage } = useWebSocket(WS_URL, {
+    shouldReconnect: (_closeEvent) => true,
+  });
+
+  return (
+    <>
+      <Box sx={{ width: "50vw", height: "700px" }}>
+        <ConsoleLog lastJsonMessage={lastJsonMessage} readyState={readyState} />
+        <ConsoleInput sendJsonMessage={sendJsonMessage} />
+      </Box>
+    </>
+  );
+}
---
+++ client/state/consoleState.ts
@@ -0,0 +1,28 @@
+import {SetterOrUpdater, atom} from 'recoil'
+
+export interface consoleMessage {
+  message: string;
+  sender: string;
+  timestamp: Date;
+}
+
+export const consoleState = atom<consoleMessage[]>({
+  key: "consoleState",
+  default: [],
+});
+
+export function isConsoleMessage(msg: consoleMessage | unknown | null): msg is consoleMessage {
+  return (
+    msg !== null &&
+    (msg as consoleMessage).message !== undefined &&
+    (msg as consoleMessage).sender !== undefined &&
+    (msg as consoleMessage).timestamp !== undefined
+  );
+}
+
+export const appendData = (
+  line: consoleMessage,
+  setLogData: SetterOrUpdater<consoleMessage[]>
+) => {
+  setLogData((log) => log.concat([line]));
+};
