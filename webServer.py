1620216f4b5b25570c91a349b0624e340968c165
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2024-03-20 00:08:47 -0400

    saveq``

--- webServer.py
+++ webServer.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, Response, jsonify
 import cv2
 from camera import camera as Camera
+from stationCommunication import transferStation
+import json
+from enum import Enum
+
 
 app = Flask(__name__)
-
-video_stream = Camera(0)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/video_feed0')
 def video_feed0():
@@ -25,7 +23,28 @@ def video_feed2():
     return Response(Camera.generate_video(Camera(2)),
     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+packetTypes = Enum('packetTypes', ['moveABS', 'moveREL', 'transferFILE'])
+
+def getMessage(jsonStr):
+    jsonObj = json.loads(jsonStr)
+    packetID = jsonObj["packetID"]
+    match packetID:
+        case :
+            pass
+        case 1:
+            pass
+        case 2: 
+            pass 
+        case _:
+            print("PACKETIDNOTFOUND\n"+jsonStr)
+
+def sendMessage(json):
+    pass
+
 
 
 if __name__ == '__main__':
+    
+    
     app.run(host='127.0.0.1', port="5000", debug=True)
+    
