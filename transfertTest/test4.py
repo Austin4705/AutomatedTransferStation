db731c4e3ab8531fe574d9ccf211fafc1ba3464e
yasudalabcornell <160793034+yasudalabcornell@users.noreply.github.com>
2024-03-21 19:20:17 -0400

    save

--- transfertTest/test4.py
+++ transfertTest/test4.py
@@ -1,31 +1,63 @@
 import numpy 
 
-i = 0
-j = 0
-arr =  numpy.zeros((4, 4))
+# i = 0
+# j = 0
+# arr =  numpy.zeros((4, 4))
 
-c = 0
-for k in range(4):
-    print(f"i{i}, j{j}")
-    c += 1
+# c = 0
+# for k in range(4):
+#     print(f"i{i}, j{j}")
+#     c += 1
+#     arr[i][j] = c
+
+#     if(k % 2 == 0):
+#         for a in range(3):
+#             j += 1
+#             print(f"i{i}, j{j}")
+#             c += 1
+#             arr[i][j] = c
+            
+            
+#     else:
+#         for a in range(3):
+#             j -= 1
+#             print(f"i{i}, j{j}")
+#             c+=1
+#             arr[i][j] = c
+            
+#     i += 1
+
+#     for a in arr:
+#         print(a)
+
+def runSquareGrid(n, func, inc):
+    i = 0
+    j = 0
+    c = 0
+    for k in range(n):
+        func(i, j, c)
+        c += inc
+        if(k % 2 == 0):
+            for a in range(n-1):
+                j += 1
+                func(i, j, c)
+                c += inc
+        else:
+            for a in range(n-1):
+                j -= 1
+                func(i, j, c)
+                c += inc   
+        i += 1
+
+n=5
+arr =  numpy.zeros((n, n))
+for a in arr:
+    print(a)
+def fu(i, j, c):
+    # print(f"i{i}, j{j}")
+    # c += 1
     arr[i][j] = c
-
-    if(k % 2 == 0):
-        for a in range(3):
-            j += 1
-            print(f"i{i}, j{j}")
-            c += 1
-            arr[i][j] = c
-            
-            
-    else:
-        for a in range(3):
-            j -= 1
-            print(f"i{i}, j{j}")
-            c+=1
-            arr[i][j] = c
-            
-    i += 1
-
-    for a in arr:
-        print(a)
+    
+runSquareGrid(5, fu, 2)
+for a in arr:
+    print(a)
