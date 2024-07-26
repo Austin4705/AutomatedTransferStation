05ef28dc400b292be30c6fd77e669c2641589296
Austin Wu <35353582+Austin4705@users.noreply.github.com>
2024-07-26 18:30:24 -0400

    push

--- src/main.py
+++ src/main.py
@@ -3,56 +3,15 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import time
 
-from transfer_station import Transfer_Station
-from socket_manager import Socket_Manager
-from camera import Camera
-import data_parser
-import web_server
+
+
+
+def orthogonally():
+    print("Making the system orthogonal")
+    # Attocube Align xy to 0, 0
+
+    # Manually move the stage to about 10-15 degrees to the right of the center point where +- is the same as the attocube
+    
 
 if __name__ == '__main__':
-    # Load enviroment variables
-    find_dotenv()
-    load_dotenv()
-    sim_test = os.getenv("sim_test") 
-    sim_test = bool(sim_test) 
-    # Wait for camera server to initialize
-    camera0 = Camera(0)
-    if sim_test:
-        print("Simulation Test: Cameras set to single source")
-    camera1 = camera0 if sim_test else Camera(1)
-    camera2 = camera0 if sim_test else Camera(2)
-
-    print("Loading Model")
-    Camera.matGMM2DTransform(Camera.mockImage)
-    print("Model Loaded")
-
-    print("Initializing Flask server")
-    flask_server_thread = threading.Thread(target=web_server.startup_flask_app)
-    flask_server_thread.daemon = True
-    flask_server_thread.start()
-    
-    print("Starting Transfer Station")
-    TRANSFER_STATION = Transfer_Station(data_parser.dispatch, Socket_Manager)
-    TRANSFER_STATION.start_serial() #Does threading creation
-
-    print("Starting socket")
-    socket_manager_thread = threading.Thread(target=Socket_Manager.start)
-    socket_manager_thread.daemon = True
-    socket_manager_thread.start()
-    
-    ts_listening_therad = threading.Thread(target=Socket_Manager.socket_dispatch_thread, args=(TRANSFER_STATION,))
-    ts_listening_therad.daemon = True
-    ts_listening_therad.start()
-
-    
-    # for i in range(1000):
-    #     time.sleep(2)
-    #     print(f"Sending {i}")
-    #     Socket_Manager.send_all("TestMessage")
-
-    input()
-    print("Finsihed Exec")
-    for thread in threading.enumerate(): 
-        print(thread.name)
-        print(thread.daemon)
-    exit(0)
+    orthogonally()
