e47589be378d0f07cb7fd3b3ff7016b74f816be4
Austin Wu <Austin4705@users.noreply.github.com>
2025-03-08 17:23:54 -0500

    save

---
+++ src/components/UnifiedLog.tsx
@@ -0,0 +1,6 @@
+  <div className="flex-grow flex flex-col border border-gray-200 rounded overflow-hidden">
+    <div 
+      ref={logContentRef}
+      className="log-content flex-grow overflow-auto thin-scrollbar"
+      style={{ minHeight: "600px", maxHeight: "600px", height: "600px" }}
+    > 
